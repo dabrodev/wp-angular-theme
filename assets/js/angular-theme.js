c0184f16e9e786c111a85066407765b4e99b775e
Damian Dabrowski <dabdamian@gmail.com>
2016-01-30 18:36:13 +0100

    refactor: chapter 4 changes

--- assets/js/angular-theme.js
+++ assets/js/angular-theme.js
@@ -1,2 +1,45 @@
-var wpApp = new angular.module('wpAngularTheme', ['ui.router', 'ngResource']);
+var wpApp = new angular.module( 'wpAngularTheme', ['ui.router', 'ngResource'] );
 
+wpApp.factory( 'Posts', function( $resource ) {
+	return $resource( appInfo.api_url + 'posts/:ID', {
+		ID: '@id'
+	})
+});
+
+wpApp.controller( 'ListCtrl', ['$scope', 'Posts', function( $scope, Posts ) {
+	console.log('ListCtrl');
+	$scope.page_title = 'Blog Listing Page';
+
+	Posts.query(function( res ) {
+		$scope.posts = res;
+	});
+	
+}]);
+
+wpApp.controller( 'DetailCtrl', ['$scope', '$stateParams', 'Posts', function( $scope, $stateParams, Posts ) {
+	console.log( $stateParams );
+	Posts.get( { ID: $stateParams.id}, function(res){
+		$scope.post = res;
+	})
+}])
+
+wpApp.config( function( $stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+		.state( 'list', {
+			url: '/',
+			controller: 'ListCtrl',
+			templateUrl: appInfo.template_directory + 'templates/list.html'
+		})
+		.state( 'detail', {
+			url: '/posts/:id',
+			controller: 'DetailCtrl',
+			templateUrl: appInfo.template_directory + 'templates/detail.html'
+		})
+});
+
+wpApp.filter( 'to_trusted', ['$sce', function( $sce ){
+	return function( text ) {
+		return $sce.trustAsHtml( text );
+	}
+}])
--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ class wp_ng_theme {
 		
 		wp_enqueue_style( 'bootstrapCSS', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', array(), '1.0', 'all' );
 		wp_enqueue_script( 'angular-core', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js', array( 'jquery' ), '1.0', false );
-        wp_enqueue_script('angular-recource', '//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-resource.js', array( 'angular-core' ), '1.0', false );
+		wp_enqueue_script( 'angular-resource', '//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-resource.js', array('angular-core'), '1.0', false );
 		wp_enqueue_script( 'ui-router', 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js', array( 'angular-core' ), '1.0', false );
 		wp_enqueue_script( 'ngScripts', get_template_directory_uri() . '/assets/js/angular-theme.js', array( 'ui-router' ), '1.0', false );
 		wp_localize_script( 'ngScripts', 'appInfo',
@@ -21,10 +21,51 @@ class wp_ng_theme {
 		);
 		
 	}
+
+	function register_new_field() {
+
+		register_api_field( 'post',
+			'my_awesome_field',
+			array(
+				'get_callback' => array( $this, 'awesome_field' ) 
+			)
+		);
+
+	}
+
+	function awesome_field( $object, $field_name, $request ) {
+
+		return 'My Awesome String';
+
+	}
+
+	function my_awesome_route() {
+
+		register_rest_route( 'wp/v2', '/roy', array(
+			'methods' => 'GET',
+			'callback' => array( $this, 'my_awesome_route_callback' )
+			)
+		);
+
+
+	}
+
+	function my_awesome_route_callback( $data ) {
+
+		$new_data = array( 'name' => 'Roy Sivan', 'age' => 29, 'location' => 'Los Angeles' );
+		$response = new WP_REST_Response( $new_data );
+
+		return $response;
+
+	}
 	
+
+
 }
 
 $ngTheme = new wp_ng_theme();
 add_action( 'wp_enqueue_scripts', array( $ngTheme, 'enqueue_scripts' ) );
+add_action( 'rest_api_init', array( $ngTheme, 'register_new_field' ) );
+add_action( 'rest_api_init', array( $ngTheme, 'my_awesome_route' ) );
 
 ?>
